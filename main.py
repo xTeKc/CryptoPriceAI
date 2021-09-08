bed8b3f6e518e45eeba94a09f8ddf737b08d973e
xTeKc <bcd3v@pm.me>
2021-09-07 20:03:49 -0400

    add more depends

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ import pandas as pd
 import pandas_datareader as web
 import datetime as dt 
 
+from sklearn.preprocessing import MinMaxScaler
+from tenserflow.keras.layers import Dense, Dropout, LSTM
+from tenserflow.keras.models import Sequential
+
