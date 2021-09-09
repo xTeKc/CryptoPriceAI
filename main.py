e34a80d2d2eed3cc47f68608d2d06b72434c2e20
xTeKc <bcd3v@pm.me>
2021-09-08 22:27:16 -0400

    finish setting up x and y training

--- main.py
+++ main.py
@@ -23,3 +23,10 @@ prediction_range = "IMPL PREDICTION RANGE" #set from creation date
 
 x_train, y_train = [], []
 
+for x in range(prediction_range, len(scaled_data)):
+	x_train.append(scaled_data[x_prediction_days:x, 0])
+	y_train.append(scaled_data[x, 0])
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
