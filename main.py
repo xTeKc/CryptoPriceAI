426366255ca8377a046c4f742fec29500e45a95c
xTeKc <bcd3v@pm.me>
2021-09-09 23:17:57 -0400

    refactor

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import pandas_datareader as web
 import datetime as dt 
 
 from sklearn.preprocessing import MinMaxScaler
-from tenserflow.keras.layers import Dense, Dropout, LSTM
-from tenserflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, LSTM
+from tensorflow.keras.models import Sequential
 
 crypto = "BTC"
 fiat = "USD"
